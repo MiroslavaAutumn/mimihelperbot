46f881d3155b90f88f49b8cba0e25f5831e0cf1f
MiroslavaAutumn <miroslavaautumn@gmail.com>
2021-01-20 13:50:29 +0300

    Added dollar and euro to ruble exchange rates

--- exchange_rates.py
+++ exchange_rates.py
@@ -1,15 +1,23 @@
 from pycoingecko import CoinGeckoAPI
+from forex_python.converter import CurrencyRates
 
-cg = CoinGeckoAPI()
+crypto = CoinGeckoAPI()
+currency = CurrencyRates()
 
 
 def get_exchange_rate():
     if what_coin.lower() == 'btc':
-        btc = cg.get_price(ids='bitcoin', vs_currencies='usd')
+        btc = crypto.get_price(ids='bitcoin', vs_currencies='usd')
         current_rate = btc['bitcoin']['usd']
     elif what_coin.lower() == 'eth':
-        eth = cg.get_price(ids='ethereum', vs_currencies='usd')
+        eth = crypto.get_price(ids='ethereum', vs_currencies='usd')
         current_rate = eth['ethereum']['usd']
+    elif what_coin.lower() == 'usd':
+        usd = round(currency.get_rate('USD', 'RUB'), 2)
+        current_rate = usd
+    elif what_coin.lower() == 'eur':
+        eur = round(currency.get_rate('EUR', 'RUB'), 2)
+        current_rate = eur
     else:
         error = 'Введенные вами данные не поддерживаются'
         return error
